aa366d69ba6481d651ef811ceb9a6a318245538b
PadamSaraogi <padamsaraogi11@gmail.com>
2025-08-30 01:38:47 +0530

    Update streamlit_app_dynamic_merged.py

--- streamlit_app_dynamic_merged.py
+++ streamlit_app_dynamic_merged.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import streamlit.components.v1 as components
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from backtest import run_backtest_simulation
